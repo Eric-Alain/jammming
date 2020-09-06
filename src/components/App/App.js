be79783b3834d9755ce849d5c8252adaf68e20cd
Eric Alain <ericalain1@hotmail.com>
2020-09-06 19:20:13 -0400

    Steps 62-65

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -54,6 +54,7 @@ class App extends React.Component {
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
   }
 
   addTrack(track) {
@@ -80,6 +81,11 @@ class App extends React.Component {
       playlistName: name
     })
   }
+
+  savePlaylist() {
+    const trackURIs = this.state.playlistTracks.map(track => track.uri);
+      
+  }
   
   render() {
     return (
@@ -97,6 +103,7 @@ class App extends React.Component {
               playlistTracks={this.state.playlistTracks}
               onRemove={this.removeTrack}
               onNameChange={this.updatePlaylistName}
+              onSave={this.savePlaylist}
             />
           </div>
         </div>
